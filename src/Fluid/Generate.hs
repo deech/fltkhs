2741ad988207208d0997500fd1e2be3e01629592
Deech <aditya.siram@gmail.com>
2019-04-05 15:11:54 -0500

    Fluid file generator no longer imports 'FLTKHS' by default.

--- src/Fluid/Generate.hs
+++ src/Fluid/Generate.hs
@@ -480,8 +480,7 @@ disclaimer =
 
 standardImports :: [String]
 standardImports =
-  [ "import Graphics.UI.FLTK.LowLevel.FLTKHS"
-  , "import qualified Graphics.UI.FLTK.LowLevel.FL as FL"
+  [ "import qualified Graphics.UI.FLTK.LowLevel.FL as FL"
   , "import Graphics.UI.FLTK.LowLevel.Fl_Types"
   , "import Graphics.UI.FLTK.LowLevel.Fl_Enumerations"
   , "import Data.Maybe"
